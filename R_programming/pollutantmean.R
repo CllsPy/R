829157765128fe107152ddd20da88f4764e311c7
cllthecoder <96326019+CllsPy@users.noreply.github.com>
2025-09-06 15:24:14 +0000

    merged

--- R_programming/pollutantmean.R
+++ R_programming/pollutantmean.R
@@ -4,13 +4,7 @@ for (data in list.files()){
   if (!exists("dataset")){
     dataset <- read.csv(data, header=TRUE)
   }
-
-  if (exists("dataset")){
-    tempory <-read.csv(data, header=TRUE)
-    dataset <-unique(rbind(dataset, tempory))
-    rm(tempory)
-  }
   
 }
 
-write.csv(dataset, "full.csv")
+write.csv(dataset, "Merged.csv")
